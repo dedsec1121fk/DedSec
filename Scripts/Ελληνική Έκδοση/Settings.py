7f12a49606e33b7c135937daf6ce889e8b26c16e
FK <dedsecfk1121@gmail.com>
2025-08-31 23:17:32 +0300

    Add files via upload

--- Scripts/Ελληνική Έκδοση/Settings.py
+++ Scripts/Ελληνική Έκδοση/Settings.py
@@ -220,7 +220,7 @@ def show_credits():
                 CREDITS
 =======================================
 Creator:dedsec1121fk
-Music Artists:BFR TEAM, PLANNO MAN, KouNouPi, ADDICTED, JAVASPA, ICE, Lefka City, , Γιάννης Βαρδής, Λαβύρινθος, Komis X, GR$, Sakin, Family Lockations, Χριστίνα Μαρκεσίνη, Δάφνη Κριθαρά, Grave_North, YungKapa, Aroy, Pi Thita, Ecostones Band, Bossikan, B-Mat, Σταμάτης Καπίρης, Lau Jr, XALIPOP, Scav, PS
+Music Artists:BFR TEAM, PLANNO MAN, KouNouPi, ADDICTED, JAVASPA, ICE, Lefka City, , Γιάννης Βαρδής, Λαβύρινθος, Komis X, GR$, Sakin, Family Lockations, Χριστίνα Μαρκεσίνη, Δάφνη Κριθαρά, Grave_North, YungKapa, Aroy, Pi Thita, Ecostones Band, Bossikan, B-Mat, Σταμάτης Καπίρης, Lau Jr, XALILOP, Scav, PS
 Art Artist:Χριστίνα Χατζηδημητρίου
 Voice Overs:Δήμητρα Ισχυροπούλου
 Technical Help:lamprouil, UKI_hunter, apoloco
