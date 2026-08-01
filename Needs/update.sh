db99d4163749d71ecd69996e6b23f164656037c4
FK <172995856+dedsec1121fk@users.noreply.github.com>
2026-08-01 15:15:58 +0300

    Add files via upload

--- Needs/update.sh
+++ Needs/update.sh
@@ -1,4 +1,4 @@
 #!/data/data/com.termux/files/usr/bin/bash
 set -u
 ROOT_DIR="$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)"
-exec bash "$ROOT_DIR/Needs/install.sh" "$@"
+exec bash "$ROOT_DIR/Needs/install.sh" --no-run "$@"
